f652503a8acb0400610a57df5b2bde60c9178761
vito.colangelo <v.colangelo@hotmail.com>
2022-05-18 15:00:50 +0200

    added linting prettier and bootstrap

---
+++ .eslintrc.js
@@ -0,0 +1,21 @@
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    es2021: true,
+    node: true
+  },
+  extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended', 'plugin:@angular-eslint/recommended', 'plugin:prettier/recommended'],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    sourceType: 'module',
+    project: ['tsconfig.json', 'tsconfig.app.json', 'tsconfig.spec.json']
+  },
+  plugins: ['@typescript-eslint', 'prettier', 'html'],
+  ignorePatterns: ['**/node_modules/**', 'build/**', 'src/polyfills.ts', 'src/index.html'],
+  rules: {
+    'prettier/prettier': 'error',
+    'arrow-body-style': 'off',
+    'prefer-arrow-callback': 'off'
+  }
+};
