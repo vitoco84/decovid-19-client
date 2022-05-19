e2da199eb089832a10b7fa3a290059b4945a31ff
vito.colangelo <v.colangelo@hotmail.com>
2022-05-19 09:46:18 +0200

    added rule for linting

--- .eslintrc.js
+++ .eslintrc.js
@@ -18,6 +18,7 @@ module.exports = {
     'arrow-body-style': 'off',
     'prefer-arrow-callback': 'off',
     '@typescript-eslint/no-var-requires': 0,
-    '@typescript-eslint/no-explicit-any': 'off'
+    '@typescript-eslint/no-explicit-any': 'off',
+    '@typescript-eslint/no-unused-vars': 'off'
   }
 };
