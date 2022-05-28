ac4f0cdda2099634fae8fc9e7da434163674904b
vito.colangelo <v.colangelo@hotmail.com>
2022-05-28 16:13:15 +0200

    fixing tests

--- src/app/component/hcert-json/hcert-json.component.spec.ts
+++ src/app/component/hcert-json/hcert-json.component.spec.ts
@@ -2,7 +2,6 @@ import {ComponentFixture, TestBed} from '@angular/core/testing';
 
 import {HcertJsonComponent} from './hcert-json.component';
 import {HttpClientTestingModule} from '@angular/common/http/testing';
-import { Decovid19Service } from "../../service/decovid19.service";
 
 describe('HcertJsonComponent', () => {
   let component: HcertJsonComponent;
