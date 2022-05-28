8038b6caa330f90360b2267ff9e77774b19f6fc4
vito.colangelo <v.colangelo@hotmail.com>
2022-05-28 09:21:59 +0200

    added coverage reporter

--- karma.conf.js
+++ karma.conf.js
@@ -15,6 +15,11 @@ module.exports = function (config) {
       },
       clearContext: false // leave Jasmine Spec Runner output visible in browser
     },
+    coverageIstanbulReporter: {
+      dir: require('path').join(__dirname, '../coverage'),
+      reports: ['html', 'lcovonly'],
+      fixWebpackSourcePaths: true
+    },
     jasmineHtmlReporter: {
       suppressAll: true // removes the duplicated traces
     },
