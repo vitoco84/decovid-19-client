e74be552bc7e9b05ec7bc0e3fe62815bb116ff29
vito.colangelo <v.colangelo@hotmail.com>
2022-09-03 13:52:39 +0200

    fix some last issues

--- src/app/component/footer/footer.component.ts
+++ src/app/component/footer/footer.component.ts
@@ -1,7 +1,17 @@
-import {Component} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-footer',
   templateUrl: './footer.component.html'
 })
-export class FooterComponent {}
+export class FooterComponent implements OnInit {
+  localHost = false;
+
+  ngOnInit(): void {
+    this.isLocalHost();
+  }
+
+  private isLocalHost(): void {
+    this.localHost = window.location.hostname === 'localhost';
+  }
+}
